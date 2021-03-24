37ccedd944bc0f78ef9482e2c2978555a897bb95
cckn <pds@enitt.co.kr>
2021-03-24 18:21:44 +0900

    setting docker

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ const a: number = 1
 const b = 2
 const c = 2
 
-const arr = new Array(100).fill(0).map((_, idx) => idx)
+const arr = new Array(100).fill(0).map((_, idx) => idx + 1000)
 
 console.log(a)
 console.log(b)
