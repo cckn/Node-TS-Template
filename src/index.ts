4a10e4e5e6273507cb1af8acc930790cce3323b8
cckn <pds@enitt.co.kr>
2021-03-24 18:03:27 +0900

    setting webpack & babel

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 const a: number = 1
-const b = 1
-const c = 1
+const b = 2
+const c = 2
+
+const arr = new Array(100).fill(0).map((_, idx) => idx)
 
 console.log(a)
 console.log(b)
 console.log(c)
+
+console.log(arr)
