095cbd88d812fad61d0b9c85b6a8bb12c5e17d38
cckn <pds@enitt.co.kr>
2021-03-26 15:37:38 +0900

    set docker & docker-compose

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,1 @@
-const a: number = 1
-const b = 2
-const c = 2
-
-const arr = new Array(100).fill(0).map((_, idx) => idx + 1000)
-
-console.log(a)
-console.log(b)
-console.log(c)
-
-console.log(arr)
+console.log('Hello World')
